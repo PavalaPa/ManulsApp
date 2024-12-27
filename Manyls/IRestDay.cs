5d733794c2ebc6a51234ad840dad6d0d1fef25f3
PavalaPa <Valentinka.pazyuk@mail.ru>
2024-12-27 08:11:07 +0300

    хз че это

--- Manyls/IRestDay.cs
+++ Manyls/IRestDay.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Manyls {
     public interface IRestDay : IFeeding {
         string RestDay(DateTime? timeOfFeeding);
